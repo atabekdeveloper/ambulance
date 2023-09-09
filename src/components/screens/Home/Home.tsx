2fea3cb97b7dce1c9a6b9e2098306077972dca59
Atabek Otepbergenov <atabek122002@gmail.com>
2023-09-09 16:13:46 +0300

    Исправил баги

--- src/components/screens/Home/Home.tsx
+++ src/components/screens/Home/Home.tsx
@@ -1,28 +1,16 @@
-import { Button, notification } from 'antd';
 import React from 'react';
 import sound from 'src/assets/audio/wrong-answer-129254.mp3';
 import useSound from 'use-sound';
 
-type NotificationType = 'success' | 'info' | 'warning' | 'error';
-
 const Home: React.FC = () => {
-  const [play] = useSound(sound);
-  const [api, contextHolder] = notification.useNotification();
-  const openNotificationWithIcon = (type: NotificationType) => {
-    api[type]({
-      message: 'Notification Title',
-      description:
-        'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
-    });
-  };
-  return (
-    <div>
-      <Button onClick={() => play()} type="primary">
-        Click
-      </Button>
-      <Button onClick={() => openNotificationWithIcon('success')}>Success</Button>
-    </div>
-  );
+  const [play] = useSound(sound, {
+    interrupt: true,
+  });
+
+  React.useEffect(() => {
+    play();
+  }, [play]);
+  return <div>Home</div>;
 };
 
 export { Home };
