b09c908c8a8334a350d69e584a93217f38de616b
Atabek Otepbergenov <atabek122002@gmail.com>
2023-09-13 15:12:36 +0300

    Исправил баги

--- src/components/screens/Home/Home.tsx
+++ src/components/screens/Home/Home.tsx
@@ -1,13 +1,5 @@
 import React from 'react';
-import sound from 'src/assets/audio/wrong-answer-129254.mp3';
-import useSound from 'use-sound';
 
-const Home: React.FC = () => {
-  const [play] = useSound(sound);
-  React.useEffect(() => {
-    play();
-  }, [play]);
-  return <div>Home</div>;
-};
+const Home: React.FC = () => <div>Home</div>;
 
 export { Home };
