12118ffd3c5811a9cd7be64c82f1b68e8583f09e
Atabek Otepbergenov <atabek122002@gmail.com>
2023-09-16 11:47:15 +0300

    Изменил ключ api yandex map

--- src/config/url.config.ts
+++ src/config/url.config.ts
@@ -1,2 +1,2 @@
 export const baseUrl = 'https://ambulance.iztileuoff.uz/api/v1';
-export const yandexApi = '03a21dbf-0bd0-4788-901d-53dabb409285';
+export const yandexApi = 'f90d801e-5706-4c4d-96df-2742aec12e8f';
