f58974cf4db29c079eaf339ef748d6b990d4d19f
Atabek Otepbergenov <atabek122002@gmail.com>
2023-11-28 12:14:15 +0300

    Изменил baseUrl

--- src/config/url.config.ts
+++ src/config/url.config.ts
@@ -1,2 +1,2 @@
-export const baseUrl = 'https://ambulance.iztileuoff.uz/api/v1';
+export const baseUrl = 'https://backend.iztileuoff.uz/api/v1';
 export const yandexApi = 'f90d801e-5706-4c4d-96df-2742aec12e8f';
